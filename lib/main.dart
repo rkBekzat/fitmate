abf93fbbd730c4f16a72063fdd3c70dd974815d6
Bekzat <rakhimbayevbekzat@gmail.com>
2023-04-01 18:08:33 +0300

    added bottom nav bar, empty pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
+import 'package:fitmate/screen/about.dart';
+import 'package:fitmate/screen/home.dart';
+import 'package:fitmate/screen/scanner.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> pages = [
+    home(),
+    about(),
+    scanner(),
+  ];
+
+  int index = 0;
   // This widget is the root of your application.
+
+  void choose(int selectedIndex){
+    setState(() {
+      index = selectedIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +39,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fitmate"),
+        ),
+        body: pages.elementAt(index),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_max_outlined),
+              label: 'home'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.info_outline),
+              label: 'about',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.camera_alt_outlined),
+              label: 'scanner'
+            ),
+          ],
+          currentIndex: index,
+          selectedItemColor: Colors.blue,
+          onTap: choose,
+        ),
+      )
     );
   }
 }
