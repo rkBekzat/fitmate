428d632d23217e6a280e4806ee94eda511f2e687
Bekzat <rakhimbayevbekzat@gmail.com>
2023-04-08 19:48:53 +0300

    added missing network

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,6 @@
+import 'package:fitmate/bloc/internet/internet_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class home extends StatelessWidget {
   const home({Key? key}) : super(key: key);
@@ -6,7 +8,25 @@ class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Home page"),
+      child: BlocConsumer<InternetCubit, InternetState>(
+        listener: (context, state) {
+          if (state is NotConnectedState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Internet not connected'),
+                backgroundColor: Colors.red,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if(state is ConnectedState) {
+            return Text("Home page");
+          } else {
+            return Text('Internet not connected');
+          }
+        },
+      ),
     );
   }
 }
